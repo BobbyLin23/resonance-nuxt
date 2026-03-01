d6b0959f1a03c982a43c3d4f5e9e442a4f785637
BobbyLin <linzhangsheng23@gmail.com>
2026-03-01 17:35:53 +0800

    feat: add ui

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="p-2">
+    <Button>
+      Click
+    </Button>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    port: 3031,
+  },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ['shadcn-nuxt', '@vueuse/nuxt', '@nuxt/fonts'],
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui',
+  },
+  fonts: {
+    families: [
+      {
+        name: 'Inter',
+        provider: 'bunny',
+      },
+      {
+        name: 'Geist_Mono',
+        provider: 'bunny',
+      },
+    ],
+  },
 })
